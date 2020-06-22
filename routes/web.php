7be1692267d1d34a10f852ee3765e03e820128c3
”yuji” <y.wk095@gmail.com>
2020-06-22 21:31:29 +0900

    first commit

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+/*Route::get('/', function () {
     return view('welcome');
-});
+});*/
 
 Route::group(['prefix' => 'admin', 'middleware' => 'auth'], function() {
      Route::get('news/create', 'Admin\NewsController@add');
